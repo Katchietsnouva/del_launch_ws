dfa0a292d47e813675bd210e2cc28c6937858a90
linux_developer <philipaswa01@gmail.com>
2023-12-30 18:21:33 +0300

    commit 4

---
+++ build/c_launch/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/allsparknouvaneonss/Desktop/del_launch_ws/src/c_launch
+# Build directory: /home/allsparknouvaneonss/Desktop/del_launch_ws/build/c_launch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
